c2980007a3a0bc9557c2818745020e0f5858bc0f
Ahileswaran <akhilleswaran@gmail.com>
2024-02-25 17:09:11 +0530

    Update

---
+++ script.php
@@ -0,0 +1,20 @@
+<?php
+$username = ""; 
+$password = ""; 
+$server = "";   
+$database = "teachersDatabase"; 
+
+$connection = new mysqli($server, $username, $password, $database);
+
+if ($connection->connect_error) {
+    die("Connection failed: " . $connection->connect_error);
+}
+
+echo "Server connected successfully";
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    
+}
+
+$connection->close();
+?>
