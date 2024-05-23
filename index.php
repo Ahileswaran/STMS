cabe62e51c5fca10e2b04b6724fc6a3bbf205e5c
Ahileswaran <akhilleswaran@gmail.com>
2024-05-23 22:53:00 +0530

    fixed profile container postition

--- index.php
+++ index.php
@@ -90,19 +90,22 @@ if($stmt->num_rows > 0) {
 
     <div class="carousel">
         <div class="carousel-item active" id="image_1">
-            <img class="animated bounceInRight carousel-img" style="background-size:cover!important" src="../STMS/images/carousel/pic1.png">
+            <img class="animated bounceInRight carousel-img" style="background-size:cover!important"
+                src="../STMS/images/carousel/pic1.png">
             <div class="row">
                 <h3 class="animated slideInLeft carousel-title mb-2">Events</h3>
             </div>
         </div>
         <div class="carousel-item">
-            <img class="animated bounceInRight carousel-img" style="background-size:cover!important" src="../STMS/images/carousel/pic1.png">
+            <img class="animated bounceInRight carousel-img" style="background-size:cover!important"
+                src="../STMS/images/carousel/pic2.png">
             <div class="row">
                 <h3 class="animated slideInLeft carousel-title mb-2">Meetings</h3>
             </div>
         </div>
         <div class="carousel-item">
-            <img class="animated bounceInRight carousel-img" style="background-size:cover!important" src="../STMS/images/carousel/pic1.png">
+            <img class="animated bounceInRight carousel-img" style="background-size:cover!important"
+                src="../STMS/images/carousel/pic2.png">
             <div class="row">
                 <h3 class="animated slideInLeft carousel-title mb-2">Celebration</h3>
             </div>
@@ -204,36 +207,36 @@ if($stmt->num_rows > 0) {
 
     </div>
 
-        <footer class="footer">
-            <div class="footer-container">
-                <div class="footer-social">
-                    <ul>
-                        <li><a href="#"><img src="imgs/social-facebook.png" alt="Facebook"></a></li>
-                        <li><a href="#"><img src="imgs/social-instagram.png" alt="Instagram"></a></li>
-                        <li><a href="#"><img src="imgs/social-youtube.png" alt="YouTube"></a></li>
-                        <li><a href="#"><img src="imgs/social-twitter.png" alt="Twitter"></a></li>
-                        <li><a href="#"><img src="imgs/social-linkedin.png" alt="LinkedIn"></a></li>
-                        <li><a href="#"><img src="imgs/social-medium.png" alt="Medium"></a></li>
-                        <li><a href="#"><img src="imgs/social-tiktok.png" alt="TikTok"></a></li>
-                    </ul>
-                </div>
-                <div class="footer-logo">
-                    <img src="images/logo-STMS.jpg" alt="Logo">
-                    <p>&copy; 2024 School Teachers Management System. All rights reserved.</p>
-                </div>
-                <div class="footer-links">
-                    <ul>
-                        <li><a href="#">Legal Stuff</a></li>
-                        <li><a href="#">Privacy Policy</a></li>
-                        <li><a href="#">Security</a></li>
-                        <li><a href="#">Website Accessibility</a></li>
-                        <li><a href="#">Manage Cookies</a></li>
-                    </ul>
-                </div>
+    <footer class="footer">
+        <div class="footer-container">
+            <div class="footer-social">
+                <ul>
+                    <li><a href="#"><img src="imgs/social-facebook.png" alt="Facebook"></a></li>
+                    <li><a href="#"><img src="imgs/social-instagram.png" alt="Instagram"></a></li>
+                    <li><a href="#"><img src="imgs/social-youtube.png" alt="YouTube"></a></li>
+                    <li><a href="#"><img src="imgs/social-twitter.png" alt="Twitter"></a></li>
+                    <li><a href="#"><img src="imgs/social-linkedin.png" alt="LinkedIn"></a></li>
+                    <li><a href="#"><img src="imgs/social-medium.png" alt="Medium"></a></li>
+                    <li><a href="#"><img src="imgs/social-tiktok.png" alt="TikTok"></a></li>
+                </ul>
             </div>
-        </footer>
-    
-        <script src="javaScript.js"></script>
-    </body>
-    
-    </html>
+            <div class="footer-logo">
+                <img src="images/logo-STMS.jpg" alt="Logo">
+                <p>&copy; 2024 School Teachers Management System. All rights reserved.</p>
+            </div>
+            <div class="footer-links">
+                <ul>
+                    <li><a href="#">Legal Stuff</a></li>
+                    <li><a href="#">Privacy Policy</a></li>
+                    <li><a href="#">Security</a></li>
+                    <li><a href="#">Website Accessibility</a></li>
+                    <li><a href="#">Manage Cookies</a></li>
+                </ul>
+            </div>
+        </div>
+    </footer>
+
+    <script src="javaScript.js"></script>
+</body>
+
+</html>
